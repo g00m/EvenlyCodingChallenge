3dd5a3585d5da2049a25cd8085bdbf9283f7759f
Etienne Negro <etienne.wojahn@me.com>
2018-11-25 13:17:30 +0100

    Initial Commit

---
+++ EvenlyCodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EvenlyCodingChallenge
+//
+//  Created by Ed Negro on 25.11.18.
+//  Copyright © 2018 Etienne Negro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
