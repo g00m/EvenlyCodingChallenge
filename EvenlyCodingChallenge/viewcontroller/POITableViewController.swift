c6a4458a21baa9de15b7d49b9be105b4836a25ed
Etienne Negro <etienne.wojahn@me.com>
2018-11-25 13:31:34 +0100

    Add POITableViewController

---
+++ EvenlyCodingChallenge/viewcontroller/POITableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  POITableViewController.swift
+//  EvenlyCodingChallenge
+//
+//  Created by Ed Negro on 25.11.18.
+//  Copyright © 2018 Etienne Negro. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
